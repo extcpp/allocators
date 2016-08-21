47a0efd4e2fa2cc7dcc3488ddef2c1f680af0b90
mic <michael.becker@rwth-aachen.de>
2016-08-21 14:21:59 +0200

    memblock satisfies the NullablePointer concept now

--- memblock.hpp
+++ memblock.hpp
@@ -10,10 +10,34 @@ namespace alloc
 		using pointer = void*;
 		using size_t = std::size_t;
 
+		memblock() noexcept = default;
+
+		memblock(void* ptr, std::size_t size)
+			: ptr{ptr}, size{size}
+		{ }
+
+		memblock(std::nullptr_t)
+			: ptr(nullptr), size{0}
+		{ }
+
+		explicit operator bool()
+		{
+			return ptr != nullptr;
+		}
+
 		void* ptr;
 		std::size_t size;
 	};
 
+	bool operator== (memblock lhs, memblock rhs)
+	{
+		return lhs.ptr == rhs.ptr && lhs.size == rhs.size;
+	}
+
+	bool operator!= (memblock lhs, memblock rhs)
+	{
+		return !(lhs == rhs);
+	}
 } // namespace alloc
 
 #endif
